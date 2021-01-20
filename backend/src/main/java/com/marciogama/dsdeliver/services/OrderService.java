34037fdd324c51c7eca4e17792728bdc3e3d3cc9
marciogama <marciogama2000@gmail.com>
2021-01-20 14:26:26 -0300

    GET /orders

---
+++ backend/src/main/java/com/marciogama/dsdeliver/services/OrderService.java
@@ -0,0 +1,25 @@
+package com.marciogama.dsdeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.marciogama.dsdeliver.dto.OrderDTO;
+import com.marciogama.dsdeliver.entities.Order;
+import com.marciogama.dsdeliver.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll() {
+		List<Order> list = repository.findOrdersWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+}
