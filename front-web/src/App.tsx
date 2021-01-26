54ca9b50b6a4807c8a759b3faf411dd9a7de8edc
marciogama <marciogama2000@gmail.com>
2021-01-26 11:17:13 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css';
 import './Navbar/index';
 import NavBar from './Navbar/index';
+import Home from './Home/index';
 
 function App() {
   return (
-    <NavBar />
+    <>
+      <NavBar />
+      <Home />
+    </>
   );
 }
 
