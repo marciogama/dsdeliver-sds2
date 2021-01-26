2f6d5e2ea09c042a6711ac041ff25f05abc9f34f
marciogama <marciogama2000@gmail.com>
2021-01-26 10:00:17 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css';
+import './Navbar/index';
+import NavBar from './Navbar/index';
 
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <NavBar />
   );
 }
 
